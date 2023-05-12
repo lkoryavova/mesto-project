f8d05bef149a16bb6dace241d4faa7bfccf06b9b
Elena Koryavova <lkoryavova@yandex.ru>
2023-05-12 18:49:56 +0300

    создан файл .gitignore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+export const popup = document.querySelector('.popup');
 const profileForm = document.forms["form-profile"];
 const cardForm = document.forms["card-form"];
 
@@ -22,3 +23,12 @@ enableValidation();
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 profileForm.addEventListener('submit', submitProfileForm);
+
+console.log('Hello, World');
+
+const numbers = [2, 3, 5];
+
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // плагин, который учит «Вебпак» работать с html-файлами 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // плагин, который каждый раз при сборке проекта будет удалять содержимое папки dist
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+        publicPath: ''
+  },
+    mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true
+  },
+    module: {
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      }
+      ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' // путь к файлу index.html
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
+
